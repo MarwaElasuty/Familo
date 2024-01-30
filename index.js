63c54352939d101c19fada81c6932ad01019272d
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-01-30 13:01:55 +0200

    yes

---
+++ index.js
@@ -0,0 +1,11 @@
+var toggle = document.querySelector(".ul-list");
+       function appear(){
+        if(toggle.style.display=='block'){
+          toggle.style.display='none';
+        }
+        else{
+          toggle.style.display='block';
+        }
+        
+
+       }
